a8598a00c17893fbb6ef49a1bde6e3cac7994094
Alessandro Vendramini <alessandrovendramini@MacBook-Pro-di-its.local>
2019-12-02 17:04:58 +0100

    complete speakers

--- app/speakers/speakers-view-model.js
+++ app/speakers/speakers-view-model.js
@@ -1,13 +1,14 @@
 const observableModule = require("tns-core-modules/data/observable");
+const data = require('./data');
+const rendata = data.map((item) => {
+    item.interventocompleto = `${item.nome} - ${item.inizio} ${item.fine}`
+    return item
+})
 
 function SpeakersViewModel() {
     const viewModel = observableModule.fromObject({
-
-        name: "Mario Bianchi",
-        azione: "Colazione 8:00 - 9:00",
-        descrizione: "caffè cornetto amaro"
+        items: rendata
     });
-
     return viewModel;
 }
 
