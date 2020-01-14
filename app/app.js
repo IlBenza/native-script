8e8bfe65c2847c3c017f89166e8155ac40795695
Alessandro Vendramini <alessandrovendramini@MacBook-Pro-di-its.local>
2020-01-14 17:32:17 +0100

    firestore add model schedule

--- app/app.js
+++ app/app.js
@@ -1,15 +1,23 @@
 const application = require("tns-core-modules/application");
 const config = require("./config");
 const MapAPI = config.google.map.provideAPIKey || "AIzaSyCW9gWe6oddduQtkscKWP2Z_Yb7tagvZr4";
+const firebase = require("nativescript-plugin-firebase/app");
+
 // Google Maps SDK API KEY
 if(application.ios) {
     GMSServices.provideAPIKey(MapAPI);
 }
 
+firebase.initializeApp({
+  iOSEmulatorFlush: true,
+  persist: false
+}).then(
+    function () {
+      console.log("firebase.init done");
+    },
+    function (error) {
+      console.log("firebase.init error: " + error);
+    }
+);
 
 application.run({ moduleName: "app-root" });
-
-/*
-Do not place any code after the application has been started as it will not
-be executed on iOS.
-*/
--- app/schedule/schedule-items-view-model.js
+++ app/schedule/schedule-items-view-model.js
@@ -1,19 +1,45 @@
 const observableModule = require("tns-core-modules/data/observable");
 const data = require('./data')
-const redata = data.map((item) => {
-    item.name = `${item.nome}`
-    item.description = `${item.inizio} - ${item.fine} ${item.luogo}`
-    return item;
-});
+const firebase = require("nativescript-plugin-firebase/app")
+const ObservableArray = require("tns-core-modules/data/observable-array").ObservableArray;
+
+let myObservableArray = new ObservableArray();
+
+const firestore = firebase.firestore()
+const schedule = firestore.collection("schedule")
+
+//console.log('COLLECTION: ', schedule)
+schedule.get().then((document) => {
+
+    //console.log('DOCUMENTO: ', document)
+    document.forEach((doc) => {
+
+        const tmp = doc.data()
+        tmp.name = `${tmp.nome}`
+        tmp.description = `${tmp.inizio} - ${tmp.fine} ${tmp.luogo}`
+        myObservableArray.push(tmp)
+        console.log('PROVA: ', tmp)
+    })
+})
+
+//Aggiunge un oggetto nel database
+/*
+schedule.add({
+    name: "San Francisco",
+    state: "CA",
+    country: "USA",
+    capital: false,
+    population: 860000
+})
+*/
 
 function ScheduleItemsViewModel() {
     const viewModel = observableModule.fromObject({
-        items: redata,
-        myGroupingfunc: function(item) {
+        items: myObservableArray,
+        myGroupingFunc: function(item) {
             return item.group
         }
     });
-
     return viewModel;
 }
 
